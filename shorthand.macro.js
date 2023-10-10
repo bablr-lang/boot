e3d0840116549337056572a9e83a792a7864beac
Conrad Buck <conartist6@gmail.com>
2023-10-10 11:49:11 -0600

    It mostly works!

--- shorthand.macro.js
+++ shorthand.macro.js
@@ -8,28 +8,77 @@ const { createMacro } = require('babel-plugin-macros');
 const { TemplateParser } = require('./lib/miniparser.js');
 const { Resolver } = require('./lib/resolver.js');
 const instruction = require('./lib/languages/instruction.js');
+const { set, parsePath, stripPathBraces } = require('./lib/utils.js');
 
 const { isArray } = Array;
 
+const isPlainObject = (v) => isObject(v) && !isArray(v);
+
 const getASTValue = (v, exprs) => {
-  if (isObject(v) && v.type?.type === 'GapNodeTag') {
-    return exprs.pop();
+  return isNull(v)
+    ? t.nullLiteral()
+    : isUndefined(v)
+    ? t.identifier('undefined')
+    : isString(v)
+    ? t.stringLiteral(v)
+    : isArray(v)
+    ? t.arrayExpression(v.map((v) => getASTValue(v, exprs)))
+    : isPlainObject(v) && !v.language
+    ? t.objectExpression(
+        Object.entries(v).map(([k, v]) => t.objectProperty(t.identifier(k), getASTValue(v))),
+      )
+    : generateNode(v, exprs);
+};
+
+const generateNodeChild = (child) => {
+  if (child.type === 'Reference') {
+    return expression.ast(`t.ref\`${child.value}\``);
+  } else if (child.type === 'Gap') {
+    return expression.ast(`t.gap\`${child.value}\``);
+  } else if (child.type === 'String') {
+    return expression.ast(`t.str\`${child.value.replace(/\\/g, '\\\\')}\``);
+  } else if (child.type === 'Trivia') {
+    return expression.ast(`t.trivia\`${child.value.replace(/\n/g, '\\n')}\``);
+  } else if (child.type === 'Escape') {
+    return expression.ast(`t.esc\`${child.value.replace(/\n/g, '\\n'.replace(/\\/g, '\\\\'))}\``);
   } else {
-    return isNull(v)
-      ? t.nullLiteral()
-      : isUndefined(v)
-      ? t.identifier('undefined')
-      : isString(v)
-      ? t.stringLiteral(v)
-      : isArray(v)
-      ? t.arrayExpression(v.map((v) => getASTValue(v, exprs)))
-      : generateEmbedded(v, exprs);
+    throw new Error(`Unkown child type ${child.type}`);
   }
 };
 
-const generateEmbedded = (node, exprs) => {
+const generateNode = (node, exprs) => {
   const resolver = new Resolver();
-  return expression.ast`t.node()`;
+  const { children, properties, language, production } = node;
+  const production_ = t.stringLiteral(production);
+  const properties_ = {};
+  const children_ = [];
+
+  for (const child of children) {
+    children_.push(generateNodeChild(child));
+
+    if (child.type === 'Reference' || child.type === 'Gap') {
+      const path = child.value;
+      const { pathIsArray, pathName } = parsePath(path);
+
+      if (child.type === 'Gap') {
+        set(properties_, path, exprs.pop());
+      } else {
+        let value = properties[pathName];
+        if (pathIsArray) {
+          value = value[resolver.eat(pathName)];
+        }
+        set(properties_, path, generateNode(value, exprs));
+      }
+    }
+  }
+
+  return expression.ast`t.node(${production_}, ${t.arrayExpression(
+    children_,
+  )}, ${t.objectExpression(
+    Object.entries(properties_).map(([key, value]) =>
+      t.objectProperty(t.identifier(key), isArray(value) ? t.arrayExpression(value) : value),
+    ),
+  )})`;
 };
 
 const gap = { type: { language: instruction.name, production: 'Call' }, attrs: {} };
@@ -46,8 +95,8 @@ const shorthandMacro = ({ references }) => {
       quasis.map((q) => q.value.raw),
       expressions.map(() => null),
     ).eval(gap);
-    // console.log(JSON.stringify(ast, undefined, 4));
-    taggedTemplate.replaceWith(generateEmbedded(ast, expressions));
+
+    taggedTemplate.replaceWith(generateNode(ast, expressions));
   }
 
   for (const reference of spam) {
