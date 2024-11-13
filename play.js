cca9d874c1f77b9099215889f08344cd8d083806
Stirling Hostetter <stirlhoss@proton.me>
2024-11-13 10:31:11 -0700

    add support for parsing JSON-like attributes

---
+++ play.js
@@ -0,0 +1,17 @@
+// const {cst} = require("@bablr/boot")
+
+// // cst`<Node b={ balancedSpan: true balancer: "}" } />`
+// cst`<Node b=[8, 4] />`
+
+/* global console */
+
+const { parse, getAgASTValue } = require('./lib/index.js');
+
+const instruction = require('./lib/languages/cstml.js');
+const { printPrettyCSTML } = require('./lib/print.js');
+
+Error.stackTraceLimit = 30;
+
+const source = `<Node b={ balancedSpan: "Tag", Balancer: { symbol: "]", balanced: true} }  />`;
+
+console.log(printPrettyCSTML(getAgASTValue(instruction, parse(instruction, 'Node', source))));
