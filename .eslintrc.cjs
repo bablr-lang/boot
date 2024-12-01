cbedca14ea3a35d62442029c40468526511457fa
Conrad Buck <conartist6@gmail.com>
2024-12-01 09:48:57 -0700

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,1 @@
-const base = require('@bablr/eslint-config-base');
-
-module.exports = {
-  ...base,
-  globals: {
-    ...base.globals,
-    require: 'readonly',
-    module: 'readonly',
-    Proxy: 'readonly',
-  },
-};
+module.exports = require('@bablr/eslint-config-base');
